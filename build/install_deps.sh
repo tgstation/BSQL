17a18925a0c36b1db37dd954efc4c67cf71a682b
Cyberboss <Cyberboss@users.noreply.github.com>
2018-06-18 15:55:57 -0400

    Try this

--- build/install_deps.sh
+++ build/install_deps.sh
@@ -3,8 +3,7 @@ set -e
 
 if [ -d "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin" ];
 then
-  echo "Using cached directory."
-  exit 0
+  echo "Using cached BYOND directory."
 else
   echo "Setting up BYOND."
   mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
@@ -14,7 +13,19 @@ else
   cd byond
   make here
   cd ~/
-  exit 0
+fi
+
+if [ -d "$HOME/MariaDB" ];
+then
+  echo "Using cached MariaDB directory."
+else
+  echo "Setting up MariaDB."
+  mkdir -p "$HOME/MariaDB"
+  curl "http://mirrors.kernel.org/ubuntu/pool/universe/m/mariadb-connector-c/libmariadb-dev_2.3.3-1_i386.deb" -o mariadb.deb
+  dpkg -x mariadb.deb extract
+  rm mariadb.deb
+  mv extract/usr/lib/i386-linux-gnu/libmariadb.so $HOME/MariaDB/
+  rm -rf extract
 fi
 
 #some variable not set correctly, panic
