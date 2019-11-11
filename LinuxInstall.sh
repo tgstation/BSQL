19eb69233674025d4ab50e70cddc6311a580bc70
Gh0sT2242 <34068333+Gh0sT2242@users.noreply.github.com>
2019-11-11 01:19:40 -0600

    shut up travis

--- LinuxInstall.sh
+++ LinuxInstall.sh
@@ -41,13 +41,43 @@ else
   cd $HOME/vcpkg
   ./bootstrap-vcpkg.sh
   ./vcpkg integrate install
-  cd $HOME/BSQL/build/
-  sudo ./install_deps.sh
-  
-  cd $HOME/vcpkg/
-  ./vcpkg install libmariadb:x86-linux
 fi
 
+if [ -d "$HOME/BYOND/byond/bin" ];
+then
+  echo "Using cached BYOND directory."
+else
+  echo "Setting up BYOND."
+  mkdir -p "$HOME/BYOND"
+  cd "$HOME/BYOND"
+  curl "http://www.byond.com/download/build/512/512.1488_byond_linux.zip" -o byond.zip
+  unzip byond.zip
+  cd byond
+  make here
+  source $HOME/BYOND/byond/bin/byondsetup
+  cd $HOME/
+fi
+if [ -f "$HOME/MariaDB/libmariadb.so.2" ] && [ -f "$HOME/MariaDB/libmariadb.so" ];
+then
+  echo "Using cached MariaDB library."
+else
+  echo "Setting up MariaDB."
+  rm -rf "$HOME/MariaDB"
+  mkdir -p "$HOME/MariaDB"
+  cp /usr/lib/i386-linux-gnu/libmariadb.so.2 $HOME/MariaDB/
+  ln -s $HOME/MariaDB/libmariadb.so.2 $HOME/MariaDB/libmariadb.so
+fi
+
+if [ -d "$HOME/vcpkg/triplets/x86-linux.cmake" ];
+then
+  echo "x86 library located"
+else
+  echo "Building Cmake x86 libraries"
+  cp $HOME/BSQL/build/x86-linux.cmake $HOME/vcpkg/triplets
+fi
+
+cd $HOME/vcpkg/
+./vcpkg install libmariadb:x86-linux
 cd $HOME/BSQL
 cmake -DCMAKE_TOOLCHAIN_FILE=$HOME/vcpkg/scripts/buildsystems/vcpkg.cmake $HOME/BSQL
 make
