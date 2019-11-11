d5834f89d5b1cf2b60e00ee876cb6d6cc4513b53
Gh0sT2242 <34068333+Gh0sT2242@users.noreply.github.com>
2019-11-11 01:14:00 -0600

    Update install_deps.sh

--- build/install_deps.sh
+++ build/install_deps.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 set -e
 
-if [ -d "$HOME/BYOND/byond/bin" ];
+if [ -d "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}/byond/bin" ];
 then
   echo "Using cached BYOND directory."
 else
   echo "Setting up BYOND."
-  mkdir -p "$HOME/BYOND"
-  cd "$HOME/BYOND"
-  curl "http://www.byond.com/download/build/512/512.1488_byond_linux.zip" -o byond.zip
+  mkdir -p "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
+  cd "$HOME/BYOND-${BYOND_MAJOR}.${BYOND_MINOR}"
+  curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip
   unzip byond.zip
   cd byond
   make here
-  source $HOME/BYOND/byond/bin/byondsetup
-  cd $HOME/
+  cd ~/
 fi
 
 if [ -f "$HOME/MariaDB/libmariadb.so.2" ] && [ -f "$HOME/MariaDB/libmariadb.so" ];
@@ -25,12 +24,4 @@ else
   mkdir -p "$HOME/MariaDB"
   cp /usr/lib/i386-linux-gnu/libmariadb.so.2 $HOME/MariaDB/
   ln -s $HOME/MariaDB/libmariadb.so.2 $HOME/MariaDB/libmariadb.so
-fi
-
-if [ -d "$HOME/vcpkg/triplets/x86-linux.cmake" ];
-then
-  echo "x86 library located"
-else
-  echo "Building Cmake x86 libraries"
-  cp $HOME/BSQL/build/x86-linux.cmake $HOME/vcpkg/triplets
 fi
